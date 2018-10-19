c97fd044df3c45106254d0d5789a74a60769c57d
Alfredo Esteban <alfredo.esteban@unosquare.com>
2018-10-19 15:04:36 -0500

    Multiplication was added

--- calculator.js
+++ calculator.js
@@ -6,5 +6,7 @@ module.exports.sum = function (number1, number2) {
 // resta
 
 // multiplicacion
-
+module.exports.mul = function (number1, number2) {
+  return number1 * number2
+}
 // division
--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const calculator = require('./calculator.js')
 const sum = calculator.sum(5, 6)
 
 console.log(sum)
+
+const mul = calculator.mul(10, 2)
+
+console.log(mul)
